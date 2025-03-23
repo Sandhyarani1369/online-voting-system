2902e06549261e432fc5b1a2c2fdb8514a2d8347
G.SANDHYA RANI <161663704+Sandhyarani1369@users.noreply.github.com>
2025-03-23 16:18:03 +0530

    Delete db_init.py

--- db_init.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:yourpassword@localhost/online_voting_system'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
